364c7efd68347fa43ccfe56eb99408e384ef8959
MrKindness235 <patriciocoitino235@gmail.com>
2023-09-21 07:00:30 -0700

    Now in real proper Python

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for c in range(97, 122):
+for c in range(97, 123):
     if c == 101 or c == 113:
         print("", end="")
-    print("{}".format(chr(c)), end="")
+    else:
+        print("{}".format(chr(c)), end="")
