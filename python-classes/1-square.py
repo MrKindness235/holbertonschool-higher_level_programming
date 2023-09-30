b1b6cb31d6a4776a23e74de1d293baee8a553158
MrKindness235 <patriciocoitino235@gmail.com>
2023-09-30 16:51:27 -0700

    Im really anxious

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This new version of the Sqaure specimen is more advanced.
+Just slightly, but a step is still a step"""
+
+
+class Square:
+    """A Square with an empty size."""
+    def __init__(self, size):
+        self.__size = size
