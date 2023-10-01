91b30c3f1b1608e19dbd5e1e85f1b1b2b0f3bf64
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-01 02:44:14 -0700

    Forgot to add the description of the new method

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,8 +5,9 @@
 class Square:
     """Defines the Square's size correctly."""
     def __init__(self, size=0):
+        """Initializes the size value."""
         if size is not int:
             raise TypeError("size must be an intger")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
