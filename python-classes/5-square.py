db354137912abdac670f632d34076b53577d1b37
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-02 06:27:41 -0700

    Gotta finish this project real fast first

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Now Square will be able to check if the size it is given is right."""
+
+
+class Square:
+    """Defines the Square's size correctly."""
+    def __init__(self, size=0):
+        """Initializes the size value."""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+ 
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This calculates the square's area."""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """This prints the Square. Is this self-reflection?"""
+        for row in range(self.__size):
+            for column in range(self.__size):
+                print("#", end="")
+            print("")
