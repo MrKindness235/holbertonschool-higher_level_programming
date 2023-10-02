e6454c6f1f5b2507517ad0c446647aef2cb00e3b
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-02 05:46:18 -0700

    Start of the new week. This will be good!

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the beginning of the Rectangle class."""
+
+
+class Rectangle:
+    """It's a rectangle."""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Creating a Rectangle class once again.
