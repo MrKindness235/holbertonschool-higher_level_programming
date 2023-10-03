cd3bd08920bcedaa3d94825de826e353f35c216d
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-03 05:41:21 -0700

    Resolved the issue were the properties were printed in inverse order

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,13 +5,13 @@
 class Rectangle:
     """It's a formful rectangle."""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) != int:
@@ -23,7 +23,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) != int:
