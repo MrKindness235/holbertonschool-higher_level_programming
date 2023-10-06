b68f3cbb2d85ee184269849138168a5975d3e8d5
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-06 09:21:16 -0700

    Now rectangles can be represented with any kind of character

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""A new step for the Rectangle class."""
+
+
+class Rectangle:
+    """It's a formful rectangle."""
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width and self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.height and self.width == 0:
+            return ""
+        wyrm = []
+        for row in range(self.__height):
+            if row > 0:
+                wyrm.append("\n")
+            for col in range(self.__width):
+                wyrm.append(f"{self.print_symbol}")
+        return "".join(wyrm)
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
