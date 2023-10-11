0b513f50962b35ca6efcdde60e99e6b4d6e9f18e
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-11 14:43:34 -0700

    New project underway!

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This lists all available atributes and methods of an object"""
+
+
+def lookup(obj):
+    """Returns list"""
+
+    return dir(obj)
---
+++ python-inheritance/README.md
@@ -0,0 +1,4 @@
+We will inherith this world
+And we cry in woe
+And we cry in awe
+For is ours, hot or cold
