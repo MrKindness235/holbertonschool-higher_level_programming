19903c49a1c824cb70642db4926bcba712e547ad
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-23 07:36:43 -0700

    Forgot to add the Null protection

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """Sorts given list and returns highest Value."""
+    if not my_list:
+        return None
     new_list = sorted(my_list, reverse=True)
-
     return new_list[0]
