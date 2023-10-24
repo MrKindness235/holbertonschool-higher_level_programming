f2c2a987cfc75ac66d154fb2d29aad86722fec9c
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-24 06:58:25 -0700

    Found the error. I should have been returning a new list from the start

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,4 +5,6 @@ def delete_at(my_list=[], idx=0):
     elif idx > len(my_list):
         return my_list
     else:
-        
+        new_list = my_list
+        new_list.remove(my_list[idx])
+        return new_list
