c1fa806a9f897860f510172430bf3ccf3237d7fd
MrKindness235 <patriciocoitino235@gmail.com>
2023-11-22 04:34:15 -0800

    Found a better implementation. I knew using a for loop wasnt necessary, but I couldnt find a function. Now I did.

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,7 +2,4 @@
 """This handy dandy function woll return the number of keys in a dictionary."""
 
 def number_keys(a_dictionary):
-    count = 0
-    for index in range(0, len(a_dictionary)):
-        count += 1
-    return count
+    return len(list(a_dictionary))
