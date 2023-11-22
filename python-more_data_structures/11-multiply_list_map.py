bc9f5f51cec9f656ee82e79e8c7a1a0c0d8d9020
MrKindness235 <patriciocoitino235@gmail.com>
2023-11-22 05:46:29 -0800

    Lambda...

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    for index in new_list:
-        new_list[index] = new_list[index] * number
+    return list(map(lambda x: x * number, my_list))
