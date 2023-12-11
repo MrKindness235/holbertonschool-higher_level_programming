526ae7297b6ed8dd6b2b6058a6f72e7a089a9d33
MrKindness235 <patriciocoitino235@gmail.com>
2023-12-11 06:10:40 -0800

    Co-authored-by: Mvigil6343 <Mvigil6343@users.noreply.github.com>

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for index in range(0, x + 1):
-        print("{}".format(my_list[index]))
+        for index in range(0, x + 1):
+        try:
+            print("{}".format(my_list[index]))
+        except IndexError:
+            break
